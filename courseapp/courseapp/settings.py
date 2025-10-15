d6bb05125b5727fc72bb23cc92ffb906f210890e
oslamelon <ctoan501@gmail.com>
2025-10-15 08:55:53 +0700

    Make database

--- courseapp/courseapp/settings.py
+++ courseapp/courseapp/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+from idlelib.autoexpand import AutoExpand
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'courseapp.'
+    'application.apps.Application'
+    'Config'
 ]
 
 MIDDLEWARE = [
@@ -75,11 +76,18 @@ WSGI_APPLICATION = 'courseapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'courseappdbv1',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': ''
     }
 }
 
+import pymysql
+pymysql.install_as_MySQLdb()
+
+AUTH_USER_MODEL = 'application.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
